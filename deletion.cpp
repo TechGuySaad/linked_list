02b1b225d47a57e9846fb1842ff8a1a380340a48
Saad <saadreh5@gmail.com>
2021-09-07 19:42:57 +0500

    second half

---
+++ deletion.cpp
@@ -0,0 +1,23 @@
+//to delete you need two pointers
+//the item between those two pointers will be deleted when
+
+struct nodeType
+{
+
+    int info;
+
+    nodeType *link;
+
+};
+
+
+int main()
+{
+nodeType *p, *q,head;
+
+q = p->link;
+
+p->link = q->link;
+
+delete q;
+}
